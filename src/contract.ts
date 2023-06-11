743f4510a37dd9f8c68af7b81021f7e985f761f4
Phil Smith <philip@philipsmith.ie>
2023-06-11 10:21:12 +0100

    Handle transferts to address zero

--- src/contract.ts
+++ src/contract.ts
@@ -9,12 +9,20 @@ import {
   updateERC20TotalBurnedFromPayBurn, 
   updateERC20TotalSupply 
 } from "./utils/burn"
+import { ERC20Burn } from "../generated/schema"
 
 export function handlePaymentBurnExecuted(event: PaymentBurnExecuted): void {
   
   if(!TOKEN_WHILTETLIST.includes(event.params.token)) {
     // Token is not currently supported in the subgraph
     return
+  }
+
+  /* Check to see if we already have a burn, maybe from a transfer event */
+  let burn = ERC20Burn.load(event.transaction.hash.toHexString());
+  if(burn != null) {
+      // burn already exists so we can ignore this event
+      return
   }
   
   // Update the ERC20 burn entity
